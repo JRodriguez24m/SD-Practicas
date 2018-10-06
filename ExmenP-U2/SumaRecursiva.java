cd027a533c19f5930e791c46aa68ad6eab2742f8
JRodriguez24m <jesusr24m@gmail.com>
2018-10-05 21:28:04 -0700

    Ejercicio 1. Suma con  recursividad

---
+++ ExmenP-U2/SumaRecursiva.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumaRecursiva{
+  public static int sum(int num){
+    if (num==1) {
+      return 1;
+    }else{
+      return num+sum(num-1);
+    }
+  }
+  public static void main(String[] args) {
+
+    System.out.println("Resultado: "+sum(9));
+  }
+}
