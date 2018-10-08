30fa0ca3f638482f2e68434fa07046c03377c0e4
JRodriguez24m <jesusr24m@gmail.com>
2018-10-08 13:45:19 -0700

    p9

--- queue-Simple3.java
+++ queue-Simple3.java
@@ -14,6 +14,19 @@ class queue_Simple3{
     }
   }
 
+  public static void bus(int val, int pos){
+    System.out.println("");
+    boolean b=false;
+    for (int i=0;i<queue.length ;i++ ) {
+      if (val==(queue[i])){
+        System.out.println("El valor ["+val+"] se encuentra en la posición ["+(i+1)+"].");
+        b=true;
+      }
+    }if (b==false) {
+      System.out.println("**No se encuentra el valor**");
+    }
+  }
+
   public static void Push (int pos, int val){
 
       if(val == 0)
@@ -32,7 +45,7 @@ class queue_Simple3{
   }
 
   public static void main(String[] args) {
-    int Sme, pos=0, val=0,f=0,mq;
+    int Sme, pos=0, val=0,f=0,mq,a;
     char o;
     do {
       System.out.print("\n*Cola Simple, (Creación,push)\n"+
@@ -40,7 +53,7 @@ class queue_Simple3{
       "  1. Crear Cola. \n" +
       "  2. Ingresar datos. \n" +
       "  3. Eliminar primero.\n"+
-      "  4. Mostrar queue\n"+
+      "  4. Mostrar daros\n"+
       "  0. salir. \n" +
       "     Opción: "); Sme=owl.nextInt();
       switch (Sme) {
@@ -79,7 +92,37 @@ class queue_Simple3{
           break;
         case 4:
             do {
-              System.out.print("  1. Ver primer elemento\n"); mq=owl.nextInt();
+              System.out.print("  1. Ver primer elemento\n"+
+                               "  2. Ver último\n"+
+                               "  3. Ver completa\n"+
+                               "  4. Buscar dato\n"+
+                               "  0. Regresar. \n"+
+                               "    Opción: "); mq=owl.nextInt();
+              switch(mq){
+                case 0:
+                    System.out.println("...");
+                  break;
+                case 1:
+                    System.out.println("Primer elemento: "+queue[0]);
+                  break;
+                case 2:
+                    System.out.println("Ultimo elemento: "+queue[pos-1]);
+                  break;
+                case 3:
+                  System.out.println("Cola completa: ");
+                    for (int i=0; i<queue.length; i++) {
+                      System.out.print("    "+queue[i]);
+                    }
+                  break;
+                case 4:
+                    System.out.print("  Ingese el valor a buscar: ");
+                      a=owl.nextInt();
+                      bus(a,pos);
+                  break;
+                default:
+                    System.out.println("Ingrese una Opcion valida...");
+                  break;
+              }
             } while (mq!=0);
           break;
 
